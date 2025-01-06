05ad3ff2d6ae6d7d00585be2a5dbd27af8c42bd4
grittyrabbit <grittyrabbit@gmail.com>
2025-01-06 15:32:59 +0100

    Update translatetogerman.py
    Removing keys

--- translatetogerman.py
+++ translatetogerman.py
@@ -2,7 +2,6 @@ from langdetect import detect, DetectorFactory
 DetectorFactory.seed = 0
 import deepl
 
-***REMOVED*** = 'c41e3483-1b4c-411f-ad34-e2063f3bfe1d'
 translator = deepl.Translator(***REMOVED***)
 
 def translate_to_german(text):
