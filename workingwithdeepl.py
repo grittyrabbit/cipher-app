37a051d47b0594119d0afdec5a954ecb95d6e6d6
grittyrabbit <grittyrabbit@gmail.com>
2025-01-06 15:31:40 +0100

    Update workingwithdeepl.py
    Removing keys

--- workingwithdeepl.py
+++ workingwithdeepl.py
@@ -1,8 +1,5 @@
 import deepl
 
-# Replace 'YOUR_API_KEY' with your actual DeepL API key
-***REMOVED*** = '8ef68d0f-aeac-47b8-820a-9da50b71e2c1:fx'
-translator = deepl.Translator(***REMOVED***)
 
 # Function to translate text
 def translate_text(text, target_language):
